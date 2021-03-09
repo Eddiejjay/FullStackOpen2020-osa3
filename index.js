69d629a18e7104134322dcb0be3b806e61958351
Eero Mäki-Kokkila <eerojmk@gmail.com>
2021-03-09 14:00:46 +0200

    3.2

--- index.js
+++ index.js
@@ -34,6 +34,13 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/api/info', (req, res) => {
+    let count = persons.length
+    res.send(`<p>Phonebook has info for ${count} now</p>
+    <p>${new Date()}`
+    )
+  })
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
